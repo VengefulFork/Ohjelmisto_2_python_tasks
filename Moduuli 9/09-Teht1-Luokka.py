7fbcb91760565e2dccef68c57bc688480e9690ef
lauri <lauri.paloheimo@live.com>
2025-03-17 11:01:30 +0200

    1st commit

---
+++ Moduuli 9/09-Teht1-Luokka.py
@@ -0,0 +1,14 @@
+class Auto :
+    rekisteritunnus = str
+    huippunopeus = int
+    tämän_hetkinen_nopeus = 0
+    kuljettu_matka = 0
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+
+
+uusi_auto = Auto("ABC-120", 142)
+
+print(f"Auton rekisteritunnus on {uusi_auto.rekisteritunnus}, huippunopeus on {uusi_auto.huippunopeus}km/h."
+      f"\nAuton tämän hetkinen nopeus on {uusi_auto.tämän_hetkinen_nopeus} km/h ja tähän menessä kulkema matka on {uusi_auto.kuljettu_matka} km")
