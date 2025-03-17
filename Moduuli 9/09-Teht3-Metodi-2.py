a5f7d88e3803f619fff3272cf9679f88ea5b0747
lauri <lauri.paloheimo@live.com>
2025-03-17 14:07:48 +0200

    Fixes

--- Moduuli 9/09-Teht3-Metodi-2.py
+++ Moduuli 9/09-Teht3-Metodi-2.py
@@ -9,12 +9,11 @@ class Auto :
         self.rekisteritunnus = rekisteritunnus
         self.huippunopeus = huippunopeus
 
-
-    def kiihdytä (self, nopeuden_muutos):
+    def kiihdytä(self, nopeuden_muutos):
         self.tämän_hetkinen_nopeus += nopeuden_muutos
-        if self.tämän_hetkinen_nopeus < 0 :
+        if self.tämän_hetkinen_nopeus < 0:
             self.tämän_hetkinen_nopeus = 0
-        elif self.tämän_hetkinen_nopeus > 142 :
+        elif self.tämän_hetkinen_nopeus > self.huippunopeus:
             self.tämän_hetkinen_nopeus = 142
         return
     def kulje (self, aika):
