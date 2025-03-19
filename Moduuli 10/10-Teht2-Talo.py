77b98eb70c9f375e69eaacf0329a332067e13f48
lauri <lauri.paloheimo@live.com>
2025-03-19 13:56:03 +0200

    1st commit

---
+++ Moduuli 10/10-Teht2-Talo.py
@@ -0,0 +1,64 @@
+class Hissi ():
+
+    def __init__(self, alin_kerros, ylin_kerros, hissin_nimi):
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
+        self.aloitus_kerros = alin_kerros
+        self.tämän_hetkinen_kerros = alin_kerros
+        self.hissin_nimi = hissin_nimi
+
+    def kerros_alas(self):
+        if self.tämän_hetkinen_kerros == self.alin_kerros :
+            print("Hissi ei voi mennä tätä kerrosta alemmaksi")
+        elif self.tämän_hetkinen_kerros > self.alin_kerros :
+            self.tämän_hetkinen_kerros -= 1
+            print(f"Hissin uusi kerros on {self.tämän_hetkinen_kerros}")
+        return
+
+    def kerros_ylös(self):
+        if self.tämän_hetkinen_kerros == self.ylin_kerros :
+            print("Hissi ei voi mennä tätä kerrosta alemmaksi")
+        elif self.tämän_hetkinen_kerros < self.ylin_kerros :
+            self.tämän_hetkinen_kerros += 1
+            print(f"Hissin uusi kerros on {self.tämän_hetkinen_kerros}")
+        return
+    def siirrä (self, kerroksen_numero) :
+        print(f"Hissin kerros on {self.tämän_hetkinen_kerros}")
+        while self.tämän_hetkinen_kerros != kerroksen_numero :
+            if self.tämän_hetkinen_kerros < kerroksen_numero :
+                self.kerros_ylös()
+            elif self.tämän_hetkinen_kerros > kerroksen_numero :
+                self.kerros_alas()
+
+        return
+
+class Talo () :
+    def __init__(self, talon_alin_kerros, talon_ylin_kerros, hissien_määrä):
+        self.talon_alin_kerros = talon_alin_kerros
+        self.talon_ylin_kerros = talon_ylin_kerros
+        self.hissien_määrä = hissien_määrä
+        self.hissit = []
+    def hissien_luonti(self):
+        b = 0
+        for a in range(self.hissien_määrä) :
+            b += 1
+            a = Hissi(self.talon_alin_kerros, self.talon_ylin_kerros, b)
+            self.hissit.append(a)
+        return
+    def talo_hissien_siirtäminen (self, hissin_numero, kohde_kerros) :
+        for a in self.hissit :
+            if hissin_numero == a.hissin_nimi :
+                print(f"Siirrät hissiä {hissin_numero}")
+                a.siirrä(kohde_kerros)
+
+
+        return
+
+
+t = Talo(1, 10, 5)
+t.hissien_luonti()
+t.talo_hissien_siirtäminen(5, 9)
+t.talo_hissien_siirtäminen(2, 5)
+
+# for a in t.hissit :
+#     print(f"{a.hissin_nimi} ja {a.tämän_hetkinen_kerros}")
