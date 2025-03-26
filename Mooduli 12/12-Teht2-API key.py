967dd2cb46dfe5a3f54298f2028951c485ab8b14
lauri <lauri.paloheimo@live.com>
2025-03-26 13:51:16 +0200

    Removed .replace()

--- Mooduli 12/12-Teht2-API key.py
+++ Mooduli 12/12-Teht2-API key.py
@@ -6,7 +6,7 @@ from api_key import api_key
 def säätila ():
     loop = True
     while loop :
-        kaupunki = input("Anna kaupungin nimi = ").capitalize().replace(" ", "")
+        kaupunki = input("Anna kaupungin nimi = ").capitalize()
         haku = f"http://api.openweathermap.org/geo/1.0/direct?q={kaupunki}&limit={1}&appid={api_key}"
         try :
             vastaus = requests.get(haku)
@@ -17,6 +17,7 @@ def säätila ():
                     lat_long = (json_vastaus[0]["lat"], json_vastaus[0]["lon"])
                     haku2 = f"https://api.openweathermap.org/data/2.5/weather?lat={lat_long[0]}&lon={lat_long[1]}&appid={api_key}&units=metric"
                     vastaus2 = requests.get(haku2).json()
+
                     print(
                         f"Kaupungissa : {kaupunki}, lämpötila on {vastaus2["main"]["temp"]} celciusta ja säätila on {vastaus2["weather"][0]["main"]}")
                     loop = False
